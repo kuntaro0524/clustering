e15169e6c94d5d6e3aef8548fc8b0f28e3456f85
Kunio Hirata@GPUmachine <kunio.hirata@riken.jp>
2023-03-28 10:11:41 +0900

    making figures for the second revision.

---
+++ 00.KAMOdendrogram/ccdendro_skew_supfig.py
@@ -0,0 +1,131 @@
+import sys
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from scipy.cluster import hierarchy
+from scipy.stats import skewnorm
+from scipy.cluster.hierarchy import fcluster
+
+# CC(apo-apo)   Average: 0.925913, variance: 0.009192, median: 0.968700
+# CC(benz-benz) Average: 0.917110, variance: 0.008674, median: 0.957200
+# CC(apo-benz)  Average: 0.907780, variance: 0.008920, median: 0.947350
+
+scale=float(sys.argv[1])
+n_total=int(sys.argv[2])
+
+n_each = int(n_total/2.0)
+
+sample_dict=[{"name":"apo-apo",  "alpha":-10,"loc":0.98,"scale":scale},
+             {"name":"apo-benz", "alpha":-10,"loc":0.97,"scale":scale},
+             {"name":"benz-benz","alpha":-10,"loc":0.98,"scale":scale}]
+
+# Figure name
+figname="alpha_%.1f_%.3f_%.3f" % (
+    sample_dict[1]['alpha'],sample_dict[1]['loc'], sample_dict[1]['scale'])
+    
+
+files=glob.glob("%s*"%figname)
+n_exist=len(files)
+if n_exist != 0:
+    index=n_exist
+    figname="%s_%02d"%(figname,index)
+
+# Dendrogram title
+title_s = "INPUT: (%s: alpha: %8.3f loc:%8.3f scale:%8.3f)(%s alpha:%8.3f loc:%8.3f scale:%8.3f)(%s: alpha:%8.3f loc:%8.3f scale:%8.3f)" \
+    % (sample_dict[0]['name'], sample_dict[0]['alpha'],sample_dict[0]['loc'], sample_dict[0]['scale'], \
+    sample_dict[1]['name'], sample_dict[1]['alpha'],sample_dict[1]['loc'], sample_dict[1]['scale'], \
+    sample_dict[2]['name'], sample_dict[2]['alpha'],sample_dict[2]['loc'], sample_dict[2]['scale'])
+
+print(title_s)
+
+def get_stat_info(cc_combination):
+    for idx,s in enumerate(sample_dict):
+        if s['name']==cc_combination:
+            return s
+
+def make_skew_random_cc(stat_dict):
+    alpha=stat_dict['alpha']
+    loc=stat_dict['loc']
+    scale=stat_dict['scale']
+    randcc=skewnorm.rvs(alpha, loc, scale)
+
+    return randcc
+
+sample_list=[]
+for i in np.arange(0,n_each):
+    sample_list.append("apo")
+for i in np.arange(0,n_each):
+    sample_list.append("benz")
+
+dis_list = []
+name_list=[]
+
+apo_apo=[]
+apo_ben=[]
+ben_ben=[]
+
+ofile=open("cc.dat","w")
+
+for idx1,s1 in enumerate(sample_list):
+    for s2 in sample_list[idx1+1:]:
+        if s1=="apo" and s2=="apo":
+            stat_dict=get_stat_info("apo-apo")
+            name_list.append("apo-apo")
+            cctmp = make_skew_random_cc(stat_dict)
+            apo_apo.append(cctmp)
+        elif s1=="benz" and s2=="benz":
+            stat_dict=get_stat_info("benz-benz")
+            name_list.append("benz-benz")
+            cctmp = make_skew_random_cc(stat_dict)
+            ben_ben.append(cctmp)
+        else:
+            stat_dict=get_stat_info("apo-benz")
+            name_list.append("apo-benz")
+            cctmp = make_skew_random_cc(stat_dict)
+            apo_ben.append(cctmp)
+
+        if cctmp>1.0:
+            cctmp=1.0
+        dist = np.sqrt(1-cctmp*cctmp)
+        ofile.write("%9.5f\n"%cctmp)
+        dis_list.append(dist)
+
+ofile.close()
+
+# Histgram of CC
+fig = plt.figure(figsize=(25,10))
+ax1=fig.add_subplot(111)
+
+# CC stats
+aaa=np.array(apo_apo)
+aba=np.array(apo_ben)
+bba=np.array(ben_ben)
+
+ax1.set_xlim(0.70,1.0)
+
+outfile=open("results.dat","w")
+outfile.write("AA(mean,std,median)=%12.5f %12.5f %12.5f\n"% (aaa.mean(), aaa.std(), np.median(aaa)))
+outfile.write("AB(mean,std,median)=%12.5f %12.5f %12.5f\n"% (aba.mean(), aba.std(), np.median(aba)))
+outfile.write("BB(mean,std,median)=%12.5f %12.5f %12.5f\n"% (bba.mean(), bba.std(), np.median(bba)))
+outfile.close()
+
+#plt.savefig("cc_dist.png")
+
+Z = hierarchy.linkage(dis_list, 'ward')
+title_result="\nAA(mean:%5.3f std:%5.3f median:%5.3f) AB(mean:%5.3f std:%5.3f median:%5.3f) BB(mean:%5.3f std:%5.3f median:%5.3f)" % \
+    (aaa.mean(), aaa.std(), np.median(aaa), \
+    aba.mean(), aba.std(), np.median(aba), \
+    bba.mean(), bba.std(), np.median(bba))
+
+print(title_result)
+plt.title(title_s+title_result)
+
+dn = hierarchy.dendrogram(Z,labels=sample_list, leaf_font_size=10)
+
+last_merge = Z[-1]  # 最後の結合を取得
+threshold = last_merge[2]  # 最後の結合でのWard距離を取得
+print("Threshold for two main clusters:", threshold)
+
+plt.savefig("%s.jpg"%figname)
+plt.show()
