ac574a975b509018527ca48dbfb4711dbea3dc12
平田邦生 <kuntaro@kunair.local>
2023-04-12 20:19:18 +0900

    List A & B was modified -> Bugs were fixed.

--- 00.KAMOdendrogram/makeCCfromCLUSTERSTXT.py
+++ 00.KAMOdendrogram/makeCCfromCLUSTERSTXT.py
@@ -20,6 +20,46 @@ def get_id_list_from_clusters(cluster_number, file_path="CLUSTERS.txt"):
 
     return id_list
 
+# ２つのクラスタIDを受け取り、CLUSTERS.txtからそのクラスタに含まれるID番号のリストを取得
+# その際、cctable.datからCCの値を取得する
+# 抜き出す条件は、cctable.datのi,jの組のうち、「iが指定した最初のクラスタIDに所属している」かつ「jが指定した2番目のクラスタIDに所属している」
+# または　「iが指定した2番目のクラスタIDに所属している」かつ「jが指定した最初のクラスタIDに所属している」
+# とする
+
+def get_cc_cross_values_from_cctable(cluster_number1, cluster_number2, clusters_file="CLUSTERS.txt", cctable_file="cctable.dat", listname="filenames.lst"):
+    id_list1 = get_id_list_from_clusters(cluster_number1, clusters_file)
+    id_list2 = get_id_list_from_clusters(cluster_number2, clusters_file)
+
+    cc_values = []
+
+    # filename_listを読み込む(リストに格納)
+    filename_list = []
+    with open(listname, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            filename_list.append(line.strip())
+
+    cctype_list=[]
+
+    with open(cctable_file, "r") as f:
+        lines = f.readlines()
+        for line in lines[1:]:
+            cols = line.split()
+            i, j = int(cols[0]), int(cols[1])
+
+            if (i in id_list1 and j in id_list2) or (i in id_list2 and j in id_list1):
+                name_i = filename_list[i]
+                name_j = filename_list[j]
+                cctype_list.append(name_i + "_" + name_j)
+                cc_values.append(float(cols[2]))
+
+    # cc_values が格納されたDataFrameを返す
+    ret = pd.DataFrame(cc_values, columns=["cc"])
+    # retにcctype_listを追加する
+    ret["cctype"] = cctype_list
+    
+    return ret
+
 def get_cc_values_from_cctable(cluster_number, clusters_file="CLUSTERS.txt", cctable_file="cctable.dat", listname="filenames.lst"):
     id_list = get_id_list_from_clusters(cluster_number, clusters_file)
 
@@ -54,7 +94,7 @@ def get_cc_values_from_cctable(cluster_number, clusters_file="CLUSTERS.txt", cct
     return ret
 
 # CCのDataFrameを受け取って、CCの分布をヒストグラムをskeewed gaussianでフィッティングする
-def fit(cc_df, cc_threshold = 0.8):
+def fit(cc_df, cc_threshold = 0.8, nbins=20):
     # CCの数値が0.8以上のものだけに限定する
     filter_condition = cc_df['cc'] >= cc_threshold
     cc_df = cc_df[filter_condition]
@@ -64,14 +104,12 @@ def fit(cc_df, cc_threshold = 0.8):
     print("sigma_data: ", sigma_data)
     # CC data array
     ccdata = cc_df['cc']
-    n_bins = int(len(ccdata) / 8)
 
     # cc type list
     cctype_list = cc_df['cctype']
-    print(cctype_list)
 
     # 初期値？
-    hist, bin_edges = np.histogram(ccdata, bins=n_bins)
+    hist, bin_edges = np.histogram(ccdata, bins=nbins)
     bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
     # skewed gaussian 関数の定義
@@ -85,7 +123,7 @@ def fit(cc_df, cc_threshold = 0.8):
     alpha_fit, loc_fit, scale_fit = popt
 
     # Histogram drawing
-    plt.hist(ccdata, bins=n_bins, color='green', alpha=0.5)
+    plt.hist(ccdata, bins=nbins, color='green', alpha=0.5, density=True, label='CC Histogram')
     plt.xlabel('CC')
     plt.ylabel('Frequency')
 
@@ -107,19 +145,13 @@ if __name__ == "__main__":
 
     cluster_numbers = sys.argv[1:]
     cc_values_list = []
+    ncluster=int(sys.argv[3])
 
-    for idx,cluster_number in enumerate(cluster_numbers):
-        ccdf = get_cc_values_from_cctable(cluster_number)
-        if idx==0:
-            rtn_df = ccdf
-        else:
-            rtn_df = pd.concat([rtn_df, ccdf], axis=0)
-
-    # ここで cc_df の長さを表示する
-    print("cc_df length: ", len(rtn_df))
+    #df1 = get_cc_values_from_cctable(cluster_number[0])
+    #df2 = get_cc_values_from_cctable(cluster_number[1])
+    df3 = get_cc_cross_values_from_cctable(cluster_numbers[0], cluster_numbers[1])
 
     #print(f"CC values for cluster {cluster_number}: {cc_values}")
-    fit(cc_df=rtn_df, cc_threshold = 0.8)
-
-    cc_data = pd.DataFrame({'Cluster_Number': cluster_numbers, 'CC_Values': cc_values_list})
+    fit(cc_df=df3, cc_threshold = 0.8, nbins=ncluster)
+    #cc_data = pd.DataFrame({'Cluster_Number': cluster_numbers, 'CC_Values': cc_values_list},ncluster)
     print(cc_data)
