ed501e3700a13f3267468150d10b62324d066419
平田邦生 <kuntaro@vu017.spring8.or.jp>
2023-12-23 10:05:57 +0900

    merged CC2Dendrogram

--- Libs/cc2dendrogram.py
+++ Libs/cc2dendrogram.py
@@ -7,86 +7,286 @@ class CC2Dendrogram:
     def __init__(self, cctable, filename_list):
         self.cctable = cctable
         self.filename_list = filename_list
+        self.isRead = False
 
         # Ratio
         self.it_ratio = 0.7
 
-    def run(self):
+    # 分岐点のWard距離の数値を表示するための関数
+    def add_distance_labels(self, ax, linkage_matrix, threshold):
+        for i, d, c in zip(linkage_matrix[:, 0], linkage_matrix[:, 2], linkage_matrix[:, 3]):
+            x = 0.5 * (self.dendrogram['icoord'][i][1] + self.dendrogram['icoord'][i][2])
+            y = d
+            if y > threshold:
+                ax.text(x, y, f'{y:.2f}', color='r')
+            else:
+                ax.text(x, y, f'{y:.2f}', color='k')
+
+    def cleanData(self):
+        # dis_listがnumpy配列であると仮定
+        # 無限大の要素をチェック
+        inf_indices = np.where(np.isinf(self.dis_list))
+        print("無限大の要素の位置：", inf_indices)
+
+        # NaNの要素をチェック
+        nan_indices = np.where(np.isnan(self.dis_list))
+        print("NaNの要素の位置：", nan_indices)
+
+        # dis_listがnumpy配列であると仮定
+        # 無限大やNaNを含むかどうかをチェック
+        if not np.all(np.isfinite(self.dis_list)):
+            print("replace inf and nan to 0")
+            # 無限大やNaNを別の値で置き換える（例えば0や平均値など）
+            self.dis_list = np.nan_to_num(self.dis_list)
+
+    def readData(self):
         with open(self.cctable) as f:
             lines = f.readlines()
-        cc_list = []
-        dis_list = []
+        self.cc_list = []
+        self.dis_list = []
 
         for line in lines[1:]:
             x = line.split()
-            cc_list.append(float(x[2]))
-            dis_list.append(np.sqrt(1 - float(x[2]) ** 2))
+            self.cc_list.append(float(x[2]))
+            self.dis_list.append(np.sqrt(1 - float(x[2]) ** 2))
 
         with open(self.filename_list) as f2:
             lines = f2.readlines()
 
-        name_list = []
+        self.name_list = []
 
         for line in lines:
-            name_list.append(line.strip())
+            self.name_list.append(line.strip())
 
-        Z = hierarchy.linkage(dis_list, 'ward')
+        self.isRead = True
 
-        # Zの情報をファイルに書き出す "z.txt"
-        ofile = open("z.txt", "w")
-        for i in range(Z.shape[0]):
-            ofile.write(str(Z[i]) + "\n")
+    def writeZinfo(self, prefix):
+        ofile = open(prefix + "_z.txt", "w")
+        for i in range(self.Z.shape[0]):
+            ofile.write(str(self.Z[i]) + "\n")
+        ofile.close()
+        # sorted
+        ofile = open(prefix + "_z_sorted.txt", "w")
+        for i in range(self.Z_sorted.shape[0]):
+            ofile.write(str(self.Z_sorted[i]) + "\n")
         ofile.close()
 
-        last_merge = Z[-1]
+    def prepZ(self):
+        # Z を計算する
+        if self.isRead == False:
+            self.readData()
+        # cleanData
+        self.cleanData()
+        self.Z = hierarchy.linkage(self.dis_list, 'ward')
+
+        last_merge = self.Z[-1]
         thresh0 = last_merge[2]
 
-        last_merge = Z[-2]
+        last_merge = self.Z[-2]
         threshold = last_merge[2]
-
+        # max_threshは、ward distanceの最大値である
         max_thresh = threshold
         print("max_thresh", thresh0)
-        new_thresh = threshold / thresh0
-
         # isomorphic thresholdを計算する (ward distance)
-        isomorphic_thresh = self.it_ratio * max_thresh
-        print("isomorphic_thresh=", isomorphic_thresh)
+        self.isomorphic_thresh = self.it_ratio * max_thresh
+        print("isomorphic_thresh=", self.isomorphic_thresh)
 
-        # isomorphic thresholdよりも小さいWard distanceを持つクラスタを抽出する
-        # それらのクラスタを構成するデータの個数をカウントする
-        # ログファイルは 'cluster.txt'とする
-        # またログファイルに、それぞれのクラスタのノードを構成するデータの名前を書き出す
-        # データの名前というのは、self.filename_listに記載されているものとする
-        # 階層的クラスタリングの結果はデータ数が少ないものから結合されているので、
-        # Zのward distanceの数値が大きいものから順に、isomorphic thresholdよりも小さいものを探す
-        # isomorphic thresholdより小さなward distanceを持つクラスタが見つかったら、
-        # そのクラスタを構成するデータの個数をカウントする
-        # そのクラスタを構成するデータの名前をログファイルに書き出す
-        # このためには再帰的にZの内容を調べる必要がある
-        # そのためには、Zの内容を調べる関数を作成する必要がある
-        # その関数の名前は、search_Z()とする
-        # search_Z()の引数は、Z, isomorphic_thresh, name_list, log_fileとする
-        # Zは、階層的クラスタリングの結果である
-        # isomorphic_threshは、isomorphic thresholdである
-        # name_listは、データの名前のリストである 
-        # log_fileは、ログファイルの名前である
-        # search_Z()は、isomorphic thresholdよりも小さいward distanceを持つクラスタを見つける
-        # そのクラスタを構成するデータの個数をカウントする
-        # そのクラスタを構成するデータの名前をログファイルに書き出す
+        self.Z_sorted = self.Z[self.Z[:,2].argsort()[::-1]]
 
-        def search_Z(Z, isomorphic_thresh, name_list, log_file):
-            # Zの内容を調べる
-            # Zの各行の内容は、[クラスタ1, クラスタ2, ward distance, クラスタを構成するデータの個数]である
-            # クラスタ1とクラスタ2は、クラスタを構成するデータの名前のインデックスである
-            # ファイル名を取得する場合には self.name_list[クラスタ1]とすれば良い（以下同文）
-            # クラスタを構成するデータの個数が2のときには、クラスタを構成するデータの名前をログファイルに書き出す
-            # 2より大きいときには、クラスタ１のインデックスについて、再帰的にZの内容を調べる
-            # クラスタ2のインデックスについても、再帰的にZの内容を調べる
-         
-        plt.figure()
-        dn = hierarchy.dendrogram(Z, labels=name_list)
-        plt.savefig("dendrogram.jpg")
+        # Z, Z_sorted をファイルに書き出す
+        self.writeZinfo("cc")
+
+    def index2name(self, index):
+        # c1_index, c2_indexがname_listのデータ数よりも大きい場合には、
+        # c1_index, c2_indexからname_listのデータ数を引いて直下のクラスタを調査する
+        # c1_indexについてトレースしていく
+        # インデックスを与えたときにそのインデックスを解析する関数を作成する
+        flist = []
+        indlist = []
+        # print("processing index:", index)
+        if index < len(self.name_list):
+            # print("reached to the file list: index=", index)
+            flist.append(self.name_list[index])
+            indlist.append(index)
+            return flist, indlist
+        else:
+            # print("Moving to the child nodes...")
+            zdata_tmp = self.Z[index - len(self.name_list)]
+            # print("Next Z:", zdata_tmp)
+            c1_index = int(zdata_tmp[0])
+            c2_index = int(zdata_tmp[1])
+            c1_name, c1_ind = self.index2name(c1_index)
+            c2_name, c2_ind = self.index2name(c2_index)
+            flist.extend(c1_name)
+            flist.extend(c2_name)
+            indlist.extend(c1_ind)
+            indlist.extend(c2_ind)
+            return flist, indlist
+        
+    def run(self):
+        if self.isRead == False:
+            self.readData()
+        self.prepZ()
+        # ソートした結果の最初のクラスタはすべてのデータを含んでいるはずなので、
+        # そのクラスタを構成するデータの個数をカウントする
+        zenma = self.Z_sorted[0]
+        c1_index = int(zenma[0])
+        c2_index = int(zenma[1])
+        ward_distance = zenma[2]
+        n_data = int(zenma[3])
+        print(c1_index, c2_index, ward_distance, n_data)
+        print(len(self.name_list))
+
+        # Convert the returned file name list into a one-dimensional array and concatenate them
+        print(">>>>>>>>>>>>>>>>>")
+        f,ff=self.index2name(c1_index)
+        print(len(ff))
+        print(f)
+        print(ff)
+        f,ff=self.index2name(c2_index)
+        print(ff)
+
+        # fcluster関数を利用してしきい値よりも小さなクラスタのデータをまとめる
+        # しきい値は、isomorphic thresholdとする
+        cluster_list = hierarchy.fcluster(self.Z, self.isomorphic_thresh, criterion='distance')
+        print("==== CLUSTER_LIST =====")
+        print(len(cluster_list),cluster_list)
+        print("==== CLUSTER_LIST =====")
+
+        # cluster_list2の各養素はself.name_listのインデックスに対応している
+        # cluster_list2 の要素数は self.name_listと同じである
+        # self.name_listを少し簡単な名前にする
+        # 上から順に、0, 1, 2, 3, ... とする
+        # このリストをself.name_list2とする
+        self.name_list2 = []
+        for i in range(len(self.name_list)):
+            self.name_list2.append(str(i))
+
+        # 続きでやりたいこと
+        # fclusterはある閾値で線を引いたときにクラスタごとにデータにインデクスをつけてくれる
+        # 例）cluster_list = [1,2,1, 3,2,3,3,1,3,4,4,4]
+        # この場合には、1,2,3,4の4つのクラスタがあることになる
+        # このクラスタのインデクスを使って最大のWard距離を持つクラスタを探す
+        # まずcluster_listの要素とクラスタの情報をリンクさせる
+        # cluster_list[i] = j とすると self.Z[i][2]のWard距離を取得することができる
+        # 同一のjを持つ要素のうち、Ward距離が最大のものを探す
+        # まず、cluster_listについて何種類あるかを調べる
+        self.dendrogram = hierarchy.dendrogram(self.Z, labels=self.name_list2, leaf_font_size=8, color_threshold=self.isomorphic_thresh)
+        individual_cluster = set(cluster_list)
+        n_groups = len(individual_cluster)
+
+        values = self.findThresholdCluster()
+        print("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+        for value in values:
+            print(value)
+            print(self.Z[value[0]])
+        print("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV")
+
+        # デンドログラムの描画
+        # self.Zを使ってデンドログラムを描画する
+        # その際に、self.name_listをラベルにする
+        # また、isomorphic thresholdを赤い線で描画する
+        # isomorphic threshold以下のクラスタは色を変える
+        # また、isomorphic threshold以下のクラスタをまとめる
+        # その際に、まとめたクラスタのデータの個数をカウントする
+        # さらに、まとめたクラスタのデータの名前をログファイルに書き出す
+        plt.figure(figsize=(10, 10))
+        plt.title('Hierarchical Clustering Dendrogram')
+        plt.xlabel('sample index')
+        plt.ylabel('distance')
+        self.dendrogram = hierarchy.dendrogram(self.Z, labels=self.name_list2, leaf_font_size=8, color_threshold=self.isomorphic_thresh)
+
+        # 特定のノードに Ward distance と cluster number を表示する
+        # values に入っているものがそれに相当する
+        # values には (index, Z[index]) が入っている
+        # index は self.Z のインデックスである
+        # Z[index] は、[c1_index, c2_index, ward_distance, n_data] である
+        # ward_distanceはこの数値を利用する
+        # 数値を書き込む座標は dendrogram['icoord'], dendrogram['dcoord'] を利用する
+        # icoord は、各ノードのX座標のリストである (4つの要素を持つリスト)
+        # dcoord は、各ノードのY座標のリストである (4つの要素を持つリスト)
+        for idx,c in enumerate(values):
+            # index_label & word distance
+            label = "%03d\n(%5.3f)"%(c[0], c[1][2])
+            # デンドログラム上の座標を取得する
+            # dcoord は、各ノードのY座標のリストであり、Ward distanceでもある
+            # このY座標を利用し、 c[1][2] と比較、差分が0.001以下の場合には
+            # X座標を取得する
+            xcode = 0
+            ycode = 0
+            for idx2,d in enumerate(self.dendrogram['dcoord']):
+                if abs(d[1] - c[1][2]) < 0.001:
+                    xcode = self.dendrogram['icoord'][idx2][1]
+                    ycode = d[1]
+                    break
+            # label を描画する
+            plt.text(xcode, ycode, label, color='k')
+
+        # self.isomorphic_thresh を描画する
+        plt.axhline(y=self.isomorphic_thresh, color='r', linestyle='--')
         plt.show()
+
+    # data_indexは self.Zのインデックスを示している
+    def findJustBeforeThresholdStepWiseFromLowerValue(self, data_index):
+        # self.Zの中にdata_indexを含まれている要素を探す
+        # その時のZのインデックスも取得する
+        # そのときのward distanceを表示する
+        results_list = []
+        for i in range(self.Z.shape[0]):
+            c1_index = int(self.Z[i][0])
+            c2_index = int(self.Z[i][1])
+            if c1_index == data_index or c2_index == data_index:
+                print("Found! index=",i, " Ward distance=", self.Z[i][2])
+                print(self.Z[i])
+                results_list.append((i,self.Z[i]))
+                tmp_ward = self.Z[i][2]
+                if tmp_ward > self.isomorphic_thresh:
+                    print("Found! But the ward distance is larger than the isomorphic threshold")
+                    return []
+                else:
+                    next_index = i + len(self.name_list)
+                    # print("Next index=", next_index)
+                    tmp_list = self.findJustBeforeThresholdStepWiseFromLowerValue(next_index)
+                    results_list.extend(tmp_list)
+                    return results_list
+
+    def findThresholdCluster(self):
+        # fclusterを利用してしきい値よりも小さなクラスタのデータをまとめる
+        # しきい値は、isomorphic thresholdとする
+        cluster_list = hierarchy.fcluster(self.Z, self.isomorphic_thresh, criterion='distance')
+        # cluster_listの要素が同じものを辞書にまとめる
+        cluster_dict = {}
+        for i,cluster_num in enumerate(cluster_list):
+            if cluster_num not in cluster_dict:
+                cluster_dict[cluster_num] = []
+            cluster_dict[cluster_num].append(i)
+
+        # cluster_dictの中でcluster_numごとに一つだけ要素を取り出す
+        # その要素をcluster_list2とする
+        cluster_list2 = []
+        for cluster_num,cluster_data in cluster_dict.items():
+            cluster_list2.append(cluster_data[0])
+
+        print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+        print(cluster_list2)
+        print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+        results_list = []
+
+        for target_index in cluster_list2:
+            print("<<<<<<<<<<<<<<< %s <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"%target_index)
+            # results は target_index を含むクラスタを下から順に検査して、isomorphic thresholdをギリギリ超えない
+            # ノードを探し、途中のノードを含むリストになっている
+            results = self.findJustBeforeThresholdStepWiseFromLowerValue(target_index)
+            # print(results)
+            # resultsの最後に入っているものが狙っているもの
+            # print(results)
+            if len(results) > 0:
+                results_list.append(results[-1])
+            else:
+                print("Not found")
+        
+        return results_list
 
 if __name__ == "__main__":
     cctable = sys.argv[1]
